3cbcc87a9d5fda8f8ea17c4df6082f7b22051ecf
Adnan Mohammed <amohammed109@myseneca.ca>
2023-11-10 12:12:21 -0500

    Added starter code to CustomerOrder Module

--- CustomerOrder.cpp
+++ CustomerOrder.cpp
@@ -1,8 +1,78 @@
+// Name: Adnan Mohammed
+// Seneca Student ID: 174731216
+// Seneca email: amohammed109@myseneca.ca
+// Date of completion: 7th November, 2023
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+
 #define _CRT_SECURE_NO_WARNINGS
 
+#include "Utilities.h"
 #include "CustomerOrder.h"
+
+using namespace std;
+
+size_t sdds::CustomerOrder::m_widthField = 0;
 
 namespace sdds
 {
+	CustomerOrder::CustomerOrder() {
 
+	}
+
+	CustomerOrder::CustomerOrder(const string& filename) {
+		Utilities util;
+	}
+
+	CustomerOrder::CustomerOrder(const CustomerOrder& rhs) {
+		throw "Error";
+	}
+
+	CustomerOrder::CustomerOrder(CustomerOrder&& rhs) noexcept {
+		*this = move(rhs);
+	}
+
+	CustomerOrder& CustomerOrder::operator=(CustomerOrder&& rhs) noexcept {
+		if (this != &rhs)
+		{
+			delete[] m_lstItem;
+
+			m_name = rhs.m_name;
+			m_product = rhs.m_product;
+			m_cntItem = rhs.m_cntItem;
+
+			rhs.m_name = "";
+			rhs.m_product = "";
+			rhs.m_cntItem = 0;
+
+			m_lstItem = rhs.m_lstItem;
+			rhs.m_lstItem = nullptr;
+		}
+		return *this;
+	}
+
+	CustomerOrder::~CustomerOrder() {
+		for (size_t i=0u; i<m_cntItem; i++)
+		{
+			delete[] m_lstItem;
+		}
+	}
+
+	bool CustomerOrder::isOrderFilled() const {
+		return true;
+	}
+
+	bool CustomerOrder::isItemFilled(const string& itemName) const {
+		return true;
+	}
+
+	void CustomerOrder::fillItem(Station& station, ostream& os) {
+
+	}
+
+	void CustomerOrder::display(ostream& os) const {
+
+	}
 }
--- CustomerOrder.h
+++ CustomerOrder.h
@@ -1,10 +1,57 @@
+// Name: Adnan Mohammed
+// Seneca Student ID: 174731216
+// Seneca email: amohammed109@myseneca.ca
+// Date of completion: 7th November, 2023
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+
 #pragma once
 #ifndef SDDS_CUSTOMER_ORDER_H
 #define SDDS_CUSTOMER_ORDER_H
 
+#include <string>
+#include"Station.h"
+
 namespace sdds
 {
+	class CustomerOrder {
+		struct Item
+		{
+			std::string m_itemName;
+			size_t m_serialNumber{ 0 };
+			bool m_isFilled{ false };
 
+			Item(const std::string& src) : m_itemName(src) {};
+		};
+		std::string m_name{};
+		std::string m_product{};
+		size_t m_cntItem{};
+		Item** m_lstItem{};
+		static size_t m_widthField;
+	public:
+		//Constructors
+		CustomerOrder();
+		CustomerOrder(const std::string& filename);
+
+		//Copy Semantics
+		CustomerOrder(const CustomerOrder& rhs);
+		CustomerOrder& operator=(const CustomerOrder& rhs) = delete;
+
+		//Move Semantics
+		CustomerOrder(CustomerOrder&& rhs) noexcept;
+		CustomerOrder& operator=(CustomerOrder&& rhs) noexcept;
+
+		//Destructor
+		~CustomerOrder();
+
+		//Instance Functions
+		bool isOrderFilled() const;
+		bool isItemFilled(const std::string& itemName) const;
+		void fillItem(Station& station, std::ostream& os);
+		void display(std::ostream& os) const;
+	};
 }
 
 #endif // !SDDS_CUSTOMER_ORDER_H
