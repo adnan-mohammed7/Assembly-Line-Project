5a89560aeed4e9772837915d8429e0cff9285835
Adnan Mohammed <amohammed109@myseneca.ca>
2023-11-09 20:40:35 -0500

    Added CustomerOrder with starter code

---
+++ CustomerOrder.cpp
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "CustomerOrder.h"
+
+namespace sdds
+{
+
+}
---
+++ CustomerOrder.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef SDDS_CUSTOMER_ORDER_H
+#define SDDS_CUSTOMER_ORDER_H
+
+namespace sdds
+{
+
+}
+
+#endif // !SDDS_CUSTOMER_ORDER_H
