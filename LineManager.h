d78ce63a248bc708a1d29865146303080bcfc83f
Adnan Mohammed <amohammed109@myseneca.ca>
2023-11-16 20:14:04 -0500

    Shifted to MS3 and added workstation and linemanager module

---
+++ LineManager.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef SDDS_LINEMANAGER_H
+#define SDDS_LINEMANAGER_H
+
+namespace sdds
+{
+
+}
+#endif // !SDDS_LINEMANAGER_H
---
+++ Workstation.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef SDDS_WORKSTATION_H
+#define SDDS_WORKSTATION_H
+
+namespace sdds
+{
+
+}
+#endif // !SDDS_WORKSTATION_H
